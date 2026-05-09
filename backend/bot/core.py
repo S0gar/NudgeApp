3bc4eaccb1cd43ad6e96108593a5027c9369aa60
S0gar <Igor.Sarmatov2@gmail.com>
2026-05-09 09:05:27 +0400

    добавил библеотеку telebot и протестировал его работу

--- backend/bot/core.py
+++ backend/bot/core.py
@@ -1,6 +1,13 @@
 import configparser
+import telebot
 
+# обращаемся к конфигу и берем оттуда токкен бота
 config = configparser.ConfigParser()
 config.read('config.ini')
+bot_token = config["BOT"]["key"]
 
-bot_key = config["BOT"]["key"]
+bot=telebot.TeleBot(bot_token)
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id, "Hello world!")
+bot.infinity_polling()
