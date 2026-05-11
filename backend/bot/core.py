fe47ed0dbc03ca1fa903858704dd8dc977536d93
sogar <Igor.Sarmatov2@gmail.com>
2026-05-11 13:13:22 +0400

    улучшение структуры проекта

--- backend/bot/core.py
+++ backend/bot/core.py
@@ -1,23 +1,33 @@
 import configparser
 import telebot
 import yaml
+import os
+
+# абсолютный путь к папке, где лежит текущий файл
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# абсолютный путь к файлу config.ini относительно текущего файла
+CONFIG_PATH = os.path.join(CURRENT_DIR, '..', '..', 'config.ini')
+
+# абсолютный путь к файлу bot_phrases.yaml относительно текущего файла
+PHRASES_PATH = os.path.join(CURRENT_DIR, 'bot phrases.yaml')
 
 # обращаемся к конфигу и берем оттуда токкен бота
-config = configparser.ConfigParser()
-config.read('config.ini')
-bot_token = config["BOT setup"]["key"]
+CONFIG = configparser.ConfigParser()
+CONFIG.read(CONFIG_PATH)
+bot_token = CONFIG["BOT setup"]["key"]
 
 # обращаемся к файлу с фразами бота
-with open('backend/bot/bot phrases.yaml', 'r', encoding='utf-8') as file:
-    phrases_config = yaml.safe_load(file)
+with open(PHRASES_PATH, 'r', encoding='utf-8') as file:
+    PHRASES_CONFIG = yaml.safe_load(file)
 
 bot=telebot.TeleBot(bot_token)
 
 # приветсвие пользователя при получении от него /start
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    greetings_message_text = phrases_config["bot_messages"]["greeting"]
+    greetings_message_text = PHRASES_CONFIG["bot_messages"]["greeting"]
     bot.send_message(message.chat.id, greetings_message_text)
 
-
-bot.infinity_polling()
+def start_bot():
+    bot.infinity_polling()
---
+++ backend/main.py
@@ -0,0 +1,7 @@
+from bot import core
+
+def main():
+    core.start_bot()
+
+if __name__ == "__main__":
+    main()
