6bd4ea45e153c4971e7a6047476447ce3ff2ef28
S0gar <Igor.Sarmatov2@gmail.com>
2026-09-15 23:33:35 +0400

    добавил логгирование для main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from backend.bot import core
+import logging
+
+
+# получение пользовательского логгера и установка уровня логирования
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.INFO)
+
+# настройка обработчика и форматировщика в соответствии с нашими нуждами
+logger_handler = logging.FileHandler(f"{__name__}.log", mode='w')
+logger_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+# добавление форматировщика к обработчику 
+logger_handler.setFormatter(logger_formatter)
+# добавление обработчика к логгеру
+_logger.addHandler(logger_handler)
 
 
 def main():
-    print("bot is starting...")
+    _logger.info("запуск бота")
     core.start_bot()
 
-
 if __name__ == "__main__":
+    _logger.info("начало работы программы")
     main()
